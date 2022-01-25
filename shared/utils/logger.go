ee2404d8cd4e5a39be0df49b0b7bc65477c01e28
Nya Candy <dev@candinya.com>
2022-01-25 21:51:03 +0800

    feat: add some utils init

--- shared/utils/logger.go
+++ shared/utils/logger.go
@@ -1,1 +1,10 @@
 package utils
+
+import "go.uber.org/zap"
+
+func InitSugaredLogger() *zap.SugaredLogger {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // ignore error (if any)
+	sugar := logger.Sugar()
+	return sugar
+}
---
+++ shared/utils/redis.go
@@ -0,0 +1,12 @@
+package utils
+
+import "github.com/go-redis/redis/v8"
+
+func InitRedis(endpoint string) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     endpoint,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	return rdb
+}
