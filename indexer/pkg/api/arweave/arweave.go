1e4808ec823ea832b2339f40eb535bd37abc4a59
Albert <albert@rss3.io>
2022-02-22 22:18:03 +0800

    feat(indexer): parse mirror article

--- indexer/pkg/api/arweave/arweave.go
+++ indexer/pkg/api/arweave/arweave.go
@@ -3,11 +3,16 @@ package arweave
 import (
 	"fmt"
 
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/types"
+
+	"github.com/valyala/fastjson"
+
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/util"
 )
 
 const arweaveEndpoint string = "https://arweave.net"
 const arweaveGraphqlEndpoint string = "https://arweave.net/graphql"
+const mirrorHost = "https://mirror.xyz/"
 
 // GetContentByTxHash gets transaction content by tx hash.
 func GetContentByTxHash(hash string) ([]byte, error) {
@@ -37,4 +42,52 @@ func GetTransactions(from, to uint64, owner string) ([]byte, error) {
 	data := fmt.Sprintf(queryVariables, from, to, owner)
 
 	return util.Post(arweaveGraphqlEndpoint, headers, data)
+}
+
+func ParseGraphqlNode(node string) (types.MirrorArticle, error) {
+	var parser fastjson.Parser
+	parsedJson, err := parser.Parse(node)
+	if err != nil {
+		return types.MirrorArticle{}, err
+	}
+
+	article := new(types.MirrorArticle)
+
+	id := parsedJson.GetStringBytes("node", "id")
+	tags := parsedJson.GetArray("node", "tags")
+	for _, tag := range tags {
+		name := tag.GetStringBytes("name")
+		value := tag.GetStringBytes("value")
+
+		v := string(value)
+		switch string(name) {
+		case "Contributor":
+			article.Author = v
+		case "Content-Digest":
+			article.Digest = v
+		case "Original-Content-Digest":
+			article.OriginalDigest = v
+		}
+		article.Link = mirrorHost + "/" + article.Author + "/" + article.OriginalDigest
+
+	}
+
+	content, err := GetContentByTxHash(string(id))
+	if err != nil {
+		return *article, err
+	}
+
+	parsedJson, err = parser.Parse(string(content))
+	if err != nil {
+		return *article, err
+	}
+
+	// title
+	article.Title = string(parsedJson.GetStringBytes("content", "title"))
+	// timestamp
+	article.TimeStamp = parsedJson.GetUint64("content", "timestamp")
+	// content
+	article.Content = string(parsedJson.GetStringBytes("content", "body")) // timestamp
+
+	return *article, nil
 }
---
+++ indexer/types/mirror_article.go
@@ -0,0 +1,19 @@
+package types
+
+import "fmt"
+
+// MirrorArticle stores all indexed articles from arweave.
+type MirrorArticle struct {
+	Title          string
+	TimeStamp      uint64
+	Content        string
+	Author         string
+	Link           string
+	Digest         string
+	OriginalDigest string
+}
+
+func (a MirrorArticle) String() string {
+	return fmt.Sprintf(`Title: %s, TimeStamp: %d, Author: %s, Link: %s, Digest: %s, OriginalDigest: %s`,
+		a.Title, a.TimeStamp, a.Author, a.Link, a.Digest, a.OriginalDigest)
+}
