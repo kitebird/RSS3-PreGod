642e90bbcaf7bf56ab2a2a3b1e624030769ecc45
Nya Candy <dev@candinya.com>
2022-02-22 23:19:14 +0800

    fix: typo

--- hub/pkg/db/model/link.go
+++ hub/pkg/db/model/link.go
@@ -6,7 +6,7 @@ import "github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/constants"
 type Link struct {
 	Base BaseModel `gorm:"embedded"`
 
-	LinkID string `gorm:"primaryKey;type:text;column:asset_id"`
+	LinkID string `gorm:"primaryKey;type:text;column:link_id"`
 	ItemID string `gorm:"type:text;column:item_id"`
 
 	RSS3ID       string           `gorm:"type:text;column:rss3_id"`
