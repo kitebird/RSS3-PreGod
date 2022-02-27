9fe9692fb6ded00d178f421cc8d613bc79a85192
Henry <hi@henry.wang>
2022-02-27 17:15:05 +0000

    feat: set resty timeout and UA

--- indexer/pkg/util/util.go
+++ indexer/pkg/util/util.go
@@ -1,10 +1,17 @@
 package util
 
-import "github.com/go-resty/resty/v2"
+import (
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
 
 func Get(url string, headers map[string]string) ([]byte, error) {
 	// Create a Resty Client
 	client := resty.New()
+	client.SetTimeout(1 * time.Second * 10)
+
+	headers["User-Agent"] = "RSS3-PreGod"
 	request := client.R().EnableTrace().SetHeaders(headers)
 
 	// Get url
@@ -13,13 +20,16 @@ func Get(url string, headers map[string]string) ([]byte, error) {
 	return resp.Body(), err
 }
 
-func Post(url string, headers map[string]string, data string) ([]byte, error) {
+func Post(url string, headers map[string]string, data string) (*resty.Response, error) {
 	// Create a Resty Client
 	client := resty.New()
+	client.SetTimeout(1 * time.Second * 10)
+
+	headers["User-Agent"] = "RSS3-PreGod"
 	request := client.R().EnableTrace().SetHeaders(headers).SetBody(data)
 
 	// Get url
 	resp, err := request.Post(url)
 
-	return resp.Body(), err
+	return resp, err
 }
