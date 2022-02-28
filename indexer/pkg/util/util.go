386e4d40023d04b4515fe2485b6a784ef6c05a74
Henry <hi@henry.wang>
2022-02-28 20:46:24 +0000

    feat: add resty PostRaw and common headers

--- indexer/pkg/util/util.go
+++ indexer/pkg/util/util.go
@@ -11,7 +11,8 @@ func Get(url string, headers map[string]string) ([]byte, error) {
 	client := resty.New()
 	client.SetTimeout(1 * time.Second * 10)
 
-	headers["User-Agent"] = "RSS3-PreGod"
+	setCommonHeader(headers)
+
 	request := client.R().EnableTrace().SetHeaders(headers)
 
 	// Get url
@@ -20,16 +21,37 @@ func Get(url string, headers map[string]string) ([]byte, error) {
 	return resp.Body(), err
 }
 
-func Post(url string, headers map[string]string, data string) (*resty.Response, error) {
+func Post(url string, headers map[string]string, data string) ([]byte, error) {
 	// Create a Resty Client
 	client := resty.New()
 	client.SetTimeout(1 * time.Second * 10)
 
-	headers["User-Agent"] = "RSS3-PreGod"
+	setCommonHeader(headers)
+
 	request := client.R().EnableTrace().SetHeaders(headers).SetBody(data)
 
-	// Get url
+	// Post url
+	resp, err := request.Post(url)
+
+	return resp.Body(), err
+}
+
+// returns raw *resty.Response for Jike
+func PostRaw(url string, headers map[string]string, data string) (*resty.Response, error) {
+	// Create a Resty Client
+	client := resty.New()
+	client.SetTimeout(1 * time.Second * 10)
+
+	setCommonHeader(headers)
+
+	request := client.R().EnableTrace().SetHeaders(headers).SetBody(data)
+
+	// Post url
 	resp, err := request.Post(url)
 
 	return resp, err
+}
+
+func setCommonHeader(headers map[string]string) {
+	headers["User-Agent"] = "RSS3-PreGod"
 }
