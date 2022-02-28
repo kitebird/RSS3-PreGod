9c3d53f54981b9b5de3805fbba7d283d2d659359
懒散兔 <zerberrabbit@gmail.com>
2022-02-28 14:10:55 +0800

    feat(poap):add poap unit tests

--- indexer/pkg/api/poap/poap_test.go
+++ indexer/pkg/api/poap/poap_test.go
@@ -3,13 +3,14 @@ package poap_test
 import (
 	"testing"
 
+	"github.com/NaturalSelectionLabs/RSS3-PreGod/indexer/pkg/api/poap"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetActions(t *testing.T) {
 	t.Parallel()
 
-	result, err := GetActions("")
+	result, err := poap.GetActions("0xBf6f8E4ae37680a60B13C2f02b6437e6737d5203")
 
 	assert.Nil(t, err)
 
