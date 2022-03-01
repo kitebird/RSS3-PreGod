f30a8cc8a474d12d0a9c4490d383a4d6846d1caa
懒散兔 <zerberrabbit@gmail.com>
2022-03-01 23:25:33 +0800

    fix(poap):add exception handling for util.Get

--- indexer/pkg/api/poap/poap.go
+++ indexer/pkg/api/poap/poap.go
@@ -21,11 +21,14 @@ func GetActions(user string) ([]types.PoapResponse, error) {
 
 	url := fmt.Sprintf("%s/actions/scan/%s",
 		endpoint, user)
-	response, _ := util.Get(url, nil)
+	response, err := util.Get(url, nil)
+	if err != nil {
+		return []types.PoapResponse{}, err
+	}
 
 	res := new([]types.PoapResponse)
 
-	err := jsoni.Unmarshal(response, &res)
+	err = jsoni.Unmarshal(response, &res)
 	if err != nil {
 		return []types.PoapResponse{}, err
 	}
