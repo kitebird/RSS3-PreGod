67abd8ed173ce86edc957fa661dc5b881e2e4bf4
lazzyRabbit <44544510+lazzyRabbit@users.noreply.github.com>
2022-03-02 16:41:02 +0800

    Feature/indexer dev (#12)
    * feat: add redis encapsulation
    
    * test: add cache unit tests
    
    * fix: ci constant
    
    * fix: should not use a global context
    
    * feat: expose context from cache
    
    * feat: use rss3/rocketmq public image
    
    * feat: add uri
    
    * feat: add more for uri
    
    * chore: update ci
    
    Co-authored-by: Henry <hi@henry.wang>
    Co-authored-by: Songkeys <songv587@gmail.com>
    Co-authored-by: Albert <albert@rss3.io>

--- shared/pkg/cache/cache.go
+++ shared/pkg/cache/cache.go
@@ -2,24 +2,192 @@ package cache
 
 import (
 	"context"
+	"errors"
+	"time"
 
 	"github.com/NaturalSelectionLabs/RSS3-PreGod/shared/pkg/config"
 	"github.com/go-redis/redis/v8"
+	jsoniter "github.com/json-iterator/go"
 )
 
-var rdb *redis.Client
+var (
+	jsoni = jsoniter.ConfigCompatibleWithStandardLibrary
+	rdb   *redis.Client
+)
 
 func Setup() error {
+	ctx := context.Background()
+
 	rdb = redis.NewClient(&redis.Options{
 		Addr:     config.Config.Redis.Addr,
 		Password: config.Config.Redis.Password,
 		DB:       config.Config.Redis.DB,
 	})
 
-	_, err := rdb.Ping(context.Background()).Result()
-	if err != nil {
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func Get(ctx context.Context, key string, data interface{}) error {
+	value, err := rdb.Get(ctx, key).Result()
+	if err == redis.Nil || err != nil {
+		return err
+	}
+
+	return jsoni.Unmarshal([]byte(value), &data)
+}
+
+func Set(ctx context.Context, key string, data interface{}, expire time.Duration) error {
+	value, err := jsoni.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	return rdb.Set(ctx, key, value, expire).Err()
+}
+
+func Exists(ctx context.Context, key string) (bool, error) {
+	exist, err := rdb.Exists(ctx, key).Result()
+
+	if err != nil {
+		return false, err
+	}
+
+	return exist == 1, nil
+}
+
+func ZAdd(ctx context.Context, key string, data interface{}, score float64) error {
+	value, err := jsoni.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if err = rdb.ZAdd(ctx, key, &redis.Z{
+		Score:  score,
+		Member: value,
+	}).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// zrange in pre-node
+func ZRevRange(ctx context.Context, key string, min string, max string, offset int64, count int64) ([]interface{}, error) {
+	res := rdb.ZRevRangeByScore(ctx, key, &redis.ZRangeBy{
+		Min:    min,
+		Max:    max,
+		Offset: offset,
+		Count:  count,
+	})
+
+	if res == nil {
+		return nil, res.Err()
+	}
+
+	result := make([]interface{}, len(res.Val()))
+
+	for i, val := range res.Val() {
+		err := jsoni.Unmarshal([]byte(val), &result[i])
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
+}
+
+// zrangeWithScore in pre-node
+func ZRevRangeWithScore(ctx context.Context, key string, min string, max string, offset int64, count int64) ([]interface{}, error) {
+	// go-redis will check for Offset and Count
+	res := rdb.ZRevRangeByScoreWithScores(ctx, key, &redis.ZRangeBy{
+		Min:    min,
+		Max:    max,
+		Offset: offset,
+		Count:  count,
+	})
+
+	if res == nil {
+		return nil, res.Err()
+	}
+
+	result := make([]interface{}, len(res.Val()))
+
+	for i, v := range res.Val() {
+		val, ok := v.Member.(string)
+
+		if ok {
+			err := jsoni.Unmarshal([]byte(val), &result[i])
+
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, errors.New("Unable to convert to string")
+		}
+	}
+
+	return result, nil
+}
+
+// never used in prenode
+func ZRem(ctx context.Context, key string, data interface{}, score float64) error {
+	value, err := jsoni.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if _, err := rdb.ZRem(ctx, key, value).Result(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SRem(ctx context.Context, key string, data interface{}) error {
+	value, err := jsoni.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if _, err := rdb.SRem(ctx, key, value).Result(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SAdd(ctx context.Context, key string, data interface{}) error {
+	value, err := jsoni.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if _, err := rdb.SAdd(ctx, key, value).Result(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SGet(ctx context.Context, key string, data interface{}) ([]interface{}, error) {
+	res := rdb.SMembers(ctx, key)
+
+	if res == nil {
+		return nil, res.Err()
+	}
+
+	result := make([]interface{}, len(res.Val()))
+
+	for i, val := range res.Val() {
+		if err := jsoni.Unmarshal([]byte(val), &result[i]); err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
 }
