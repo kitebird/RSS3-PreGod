64d230a46b84e95dbc1f805e579d1f5232cd6f44
KallyDev <kallydev@gmail.com>
2022-03-07 18:12:40 +0800

    fix(hub): incorrect field type

--- hub/pkg/db/model/object.go
+++ hub/pkg/db/model/object.go
@@ -1,8 +1,8 @@
 package model
 
 import (
+	datatype "github.com/NaturalSelectionLabs/RSS3-PreGod/hub/pkg/db/model/datatype"
 	"github.com/lib/pq"
-	"gorm.io/datatypes"
 )
 
 // `object` model.
@@ -10,11 +10,11 @@ type Object struct {
 	ObjectID string `gorm:"primaryKey;type:text;column:object_id"` // uuid
 	AlterID  string `gorm:"type:text;column:alter_id"`             // alternative readable id for item
 
-	Title       string         `gorm:"type:text"`
-	Summary     string         `gorm:"type:text"`
-	Authors     pq.StringArray `gorm:"type:text[]"`
-	Tags        pq.StringArray `gorm:"type:text[]"`
-	Attachments datatypes.JSON `gorm:"type:jsonb"`
+	Title       string               `gorm:"type:text"`
+	Summary     string               `gorm:"type:text"`
+	Authors     pq.StringArray       `gorm:"type:text[]"`
+	Tags        pq.StringArray       `gorm:"type:text[]"`
+	Attachments datatype.Attachments `gorm:"type:jsonb"`
 
 	BaseModel `gorm:"embedded"`
 }
