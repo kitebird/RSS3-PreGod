5191423707fcee5f16dd79279ffbb381ce868e83
Albert <albert@rss3.io>
2022-03-08 13:14:44 +0800

    feat(indexer): add GetNFTs for arbitrum

--- indexer/pkg/api/arbitrum/type.go
+++ indexer/pkg/api/arbitrum/type.go
@@ -1,5 +1,23 @@
 package arbitrum
 
+import "fmt"
+
+type NFTItem struct {
+	TokenAddress string `json:"token_address"`
+	TokenId      string `json:"token_id"`
+	ContractType string `json:"contract_type"`
+	Name         string `json:"name"`
+	Symbol       string `json:"symbol"`
+	TokenURI     string `json:"token_uri"`
+	MetaData     string `json:"metadata"`
+	Valid        bool   `json:"valid"`
+}
+
+func (i NFTItem) String() string {
+	return fmt.Sprintf(`TokenAddress: %s, TokenId: %s, Type: %s, Name: %s, Symbol: %s, TokenURI: %s`,
+		i.TokenAddress, i.TokenId, i.ContractType, i.Name, i.Symbol, i.TokenURI)
+}
+
 type NFTTransferItem struct {
 	Address   string
 	Name      string
