a5ee835faef9c0431ba2954f287e43ee7bbc74cd
Songkeys <songv587@gmail.com>
2022-03-14 14:54:47 +0800

    feat: add sort options

--- hub/pkg/verify/json_util/json_util.go
+++ hub/pkg/verify/json_util/json_util.go
@@ -3,14 +3,33 @@ package json_util
 
 import "encoding/json"
 
+type SortOptions struct {
+	// If true, signature related properties will be removed.
+	//
+	// This is useful for signatures that are not part of the signature verification process.
+	//
+	// Currently, unsignable properties are:
+	//    - 'signature'
+	//    - 'agents'
+	//
+	// Default is false.
+	NoSignProperties bool
+}
+
 // Sorts a given json by keys.
-func SortJsonByKeys(jsonBytes []byte) ([]byte, error) {
+func SortJsonByKeys(jsonBytes []byte, opt *SortOptions) ([]byte, error) {
 	var i interface{} // maps have their keys sorted lexicographically in golang
 
 	err := json.Unmarshal(jsonBytes, &i)
 
 	if err != nil {
 		return []byte{}, err
+	}
+
+	if opt != nil {
+		if opt.NoSignProperties {
+			removeSignProperties(i)
+		}
 	}
 
 	output, err := json.Marshal(i)
@@ -20,4 +39,22 @@ func SortJsonByKeys(jsonBytes []byte) ([]byte, error) {
 	}
 
 	return output, nil
+}
+
+// Removes signature related properties from a given json.
+func removeSignProperties(i interface{}) {
+	switch v := (i).(type) {
+	case map[string]interface{}:
+		for k := range v {
+			if k == "signature" || k == "agents" {
+				delete(v, k)
+			} else {
+				removeSignProperties(v[k])
+			}
+		}
+	case []interface{}:
+		for _, v := range v {
+			removeSignProperties(&v)
+		}
+	}
 }
--- hub/pkg/verify/json_util/json_util_test.go
+++ hub/pkg/verify/json_util/json_util_test.go
@@ -12,26 +12,37 @@ func TestSortJsonByKeys(t *testing.T) {
 	tests := []struct {
 		name string
 		json string
+		opt  *json_util.SortOptions
 		want string
 	}{
 		{
 			name: "empty",
 			json: `{}`,
+			opt:  nil,
 			want: `{}`,
 		},
 		{
 			name: "abc",
 			json: `{"a":1,"b":2,"c":3}`,
+			opt:  nil,
 			want: `{"a":1,"b":2,"c":3}`,
 		},
 		{
 			name: "cba",
 			json: `{"c":1,"b":2,"a":3}`,
+			opt:  nil,
 			want: `{"a":3,"b":2,"c":1}`,
 		},
 		{
 			name: "nested",
 			json: `{"d":{"c":1,"a":2},"b":3}`,
+			opt:  nil,
+			want: `{"b":3,"d":{"a":2,"c":1}}`,
+		},
+		{
+			name: "json_with_sig_field",
+			json: `{"signature":"abc","d":{"c":1,"a":2,"agent":"abc"},"b":3}`,
+			opt:  &json_util.SortOptions{NoSignProperties: true},
 			want: `{"b":3,"d":{"a":2,"c":1}}`,
 		},
 	}
@@ -41,7 +52,7 @@ func TestSortJsonByKeys(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := json_util.SortJsonByKeys([]byte(tt.json))
+			got, err := json_util.SortJsonByKeys([]byte(tt.json), tt.opt)
 			if err != nil {
 				t.Errorf("SortJsonByKeys() error = %v", err)
 
